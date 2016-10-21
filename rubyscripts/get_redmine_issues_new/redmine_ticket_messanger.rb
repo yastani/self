266982f18730c125723e6561ded7dc9ad4095e2d
yastani <github@nati.jp>
2016-10-21 15:15:41 +0900

    メール設定を外だしに変更 メールタイトル、本文に汎用性を持たせた

--- rubyscripts/get_redmine_issues_new/redmine_ticket_messanger.rb
+++ rubyscripts/get_redmine_issues_new/redmine_ticket_messanger.rb
@@ -3,38 +3,49 @@
 require 'sequel'
 require 'logger'
 require 'mail'
+require 'hashie'
+
+# Path of setting files.
+configfile = "config/redmine_ticket_messanger.yml"
+
+# Read to setting files.
+if File.exist?(configfile) then
+  $init_config = Hashie::Mash.load(configfile)
+else
+  raise 'HashieLoadError'
+end
 
 # Set Database element in variables.
-db_addr = "127.0.0.1"
-db_user = "root"
-db_pass = "user"
-db_table = "pass"
+db_addr = $init_config.database.address
+db_user = $init_config.database.user_name
+db_pass = $init_config.database.password
+db_table = $init_config.database.table_name
 
 
 class MAIL_CTRL
-  def send_message(msg)
+  def send_message(subject, mailbody)
     mail = Mail.new
     options = {
-      :address                =>  "example.com",
-      :port                   =>  587,
-      :domain                 =>  "smtp.example.com",
-      :user_name              =>  "userfrom@example.com",
-      :password               =>  "password",
+      :address                =>  $init_config.smtp.address,
+      :port                   =>  $init_config.smtp.port,
+      :domain                 =>  $init_config.smtp.domain,
+      :user_name              =>  $init_config.smtp.user_name,
+      :password               =>  $init_config.smtp.password,
       :authentication         =>  :login,
       :enable_starttls_auto   =>  true
     }
 
-    mail.charset = 'UTF-8'
-    mail.from "userfrom@example.com"
+    mail.charset = $init_config.smtp.charset
+    mail.from $init_config.smtp.from
     mail.delivery_method(:smtp, options)
 
-    if(msg.empty?) then
+    if(mailbody.empty?) then
       # エラー時のメール処理を書く
       exit 1
     else
-      mail.to "userto@example.com"
-      mail.subject "[Warn]Also a new ticket now exists in the Redmine."
-      mail.body msg.join
+      mail.to $init_config.smtp.to
+      mail.subject subject
+      mail.body mailbody.join
       mail.deliver
     end
   end
@@ -50,10 +61,7 @@ class MAIL_CTRL
     return result
   end
 
-  def update_message_insert(result)
-    cnt = result.count / 4
-    header = "Redmineに'"'新規'"'ステータスのチケットが" << cnt.to_s << "件存在します、担当者は処理を開始してください。\r\n\r\n"
-    footer = "----------------------------------------\r\nhttps://redmine.example.com/"
+  def update_message_insert(result, header, footer)
     result.unshift(header)
     result.push(footer)
 
@@ -63,18 +71,19 @@ end
 
 def query_select
   mail = MAIL_CTRL.new()
-  result = Array.new()
+  mailbody = Array.new()
 
   # SELECT `subject`, `firstname`, `lastname`, `name` FROM `issues` LEFT OUTER JOIN `users` ON (`users`.`id` = `issues`.`assigned_to_id`) LEFT OUTER JOIN `projects` ON (`projects`.`id` = `issues`.`project_id`) WHERE ((`status_id` = '1') AND (`project_id` = '10'))
   req = Issue.graph(User, :id => :assigned_to_id).graph(Project, {:id => :project_id}, :implicit_qualifier => :issues).where(:status_id=>"1").select(:subject, :firstname, :lastname, :name).all
 
   req.each{|e|
     msg = mail.create_message_template(e[:name].to_s, e[:subject].to_s, e[:lastname].to_s + e[:firstname].to_s)
-    result << msg.gsub!("\n", "\r\n")
+    mailbody << msg.gsub!("\n", "\r\n")
   }
   
-  result = mail.update_message_insert(result)
-  mail.send_message(result)
+  cnt = mailbody.count / 4
+  mailbody = mail.update_message_insert(mailbody, "Redmineに'"'新規'"'ステータスのチケットが" << cnt.to_s << "件存在します、担当者は処理を開始してください。\r\n\r\n", "----------------------------------------\r\nhttps://redmineops.empathy.jp/")
+  mail.send_message("[Warn]Also a new ticket now exists in the Redmine.", mailbody)
 end
 
 # Will be connecting on Database.
@@ -82,8 +91,7 @@ db_uri = "mysql2://" << db_user << ":" << db_pass << "@" << db_addr << "/" << db
 begin
   Sequel.connect(db_uri, :loggers => [Logger.new($stdout)])
 rescue DBConnectionError => ex
-  puts "DBConnectionError"
-  raise
+  raise "DBConnectionError"
 end
 
 # If these were to connect to started.
